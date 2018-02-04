3ffe93835d04e73f8eb8f11bc7fc0a829e816a00
yu <86839868@qq.com>
2018-02-04 23:50:20 +0800

    守护程序允许兼容所有用户

--- minerInstall/daemon.sh
+++ minerInstall/daemon.sh
@@ -4,7 +4,7 @@ while :
 do
   stillRunning=$(ps -ef |grep "bcdn" |grep -v "grep")
   if [ ! -n "$stillRunning" ] ; then
-  	cd /root/opt/M_BerryMiner_ubuntu_v1_0/server/
+  	cd ~/opt/M_BerryMiner_ubuntu_v1_0/server/
   	nohup ./bcdn &
   fi
 sleep 2 
